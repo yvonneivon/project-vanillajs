ef15878edd75347e1f7897f78bbc6729a7400d20
Yvonne Farhani Armelia Arman <fonny.armelia@gmail.com>
2020-07-13 09:38:31 +0700

    Add delete function and css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo(event){
@@ -32,4 +33,23 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
         //clear todo input value
     todoInput.value = "";
+}
+
+function deleteCheck(event){
+    const item = event.target;
+        //delete todo
+    if (item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        //animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+
+        //check mark
+    if (item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
